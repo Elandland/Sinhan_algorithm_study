8d315bd419e40c00916c385e40f8e952c72fa486
Elandland <bodan99@gmail.com>
2024-04-15 18:30:22 +0900

    Feat. boj_1654 input

---
+++ java/chansol/boj_1654/boj_1654.java
@@ -0,0 +1,24 @@
+package chansol.boj_1654;
+
+import java.io.*;
+import java.util.Scanner;
+public class boj_1654 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int lan_count = sc.nextInt();
+        int lan_need = sc.nextInt();
+        int[] lan = new int[lan_count];
+
+        for(int i=0; i<lan_count; i++){
+            lan[i] = sc.nextInt();
+        }
+
+
+
+
+    }
+
+
+
+}
