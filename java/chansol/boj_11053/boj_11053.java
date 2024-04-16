827917621dde7ac666e667b3726cef5cf926cea5
Elandland <bodan99@gmail.com>
2024-04-17 08:19:04 +0900

    Feat. boj_11053 input

---
+++ java/chansol/boj_11053/boj_11053.java
@@ -0,0 +1,23 @@
+package chansol.boj_11053;
+
+import java.io.*;
+import java.util.*;
+public class boj_11053 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+
+
+    }
+
+}
