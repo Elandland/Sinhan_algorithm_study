b2e52b4387604124922d4afc2336c4ff6b7cf43b
Elandland <bodan99@gmail.com>
2024-04-18 16:43:37 +0900

    Feat. 도넛 대충 어떻게 풀지

--- java/chansol/donut/donut.java
+++ java/chansol/donut/donut.java
@@ -1,10 +1,15 @@
 package chansol.donut;
 
-public class donut {
+class donut{
     public int[] solution(int[][] edges) {
         int[] answer = {};
+        int a,b=0;
 
+        for(int i=0; i<edges.length; i++){
+            a = edges[i][0];
+            b = edges[i][1];
 
+        }
         //막대인지 확인(그냥 막대)
 
         //8자 인지 확인(한 정점이 들어오는게 2개 나가는게 2개 나머지 1개씩)
@@ -12,8 +17,8 @@ public class donut {
         //도넛인지 확인(도넛. 간선 보낸 노드한테 다시 돌아옴)
 
 
-        //생성한 정점은 그래프의 갯수 만큼 간선을 보낸다. ->이걸로 정점 판단.
+
+        //생성한 정점은 그래프의 갯수 만큼 간선을 보낸다.
         return answer;
     }
-
 }
