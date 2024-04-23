a85288ef1247856b0a33949eadbe607cdae1158d
soyalattee <uyyu1734@gmail.com>
2024-04-23 23:30:20 +0900

    feat: 백준 계단 오르기 2579 문제  틀림

--- soya/src/boj_2579.ts
+++ soya/src/boj_2579.ts
@@ -8,13 +8,30 @@ import * as fs from "fs";
 //4. 입력값이 첫 번째 줄에는 입력 값의 길이(n), 두 번째 줄에 공백으로 구분된 입력값이 주어질 때
 // const [n, input] = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 // const inputArr = input.trim().split(" ")
-const [n, input] = fs
-  .readFileSync("./dummy/input_2602.txt")
+const [n, ...inputArr] = fs
+  .readFileSync("./dummy/input_2579.txt")
   .toString()
   .trim()
   .split("\n");
-const inputArr = input.trim().split(" ");
 
-function solution() {}
+function solution() {
+  let stairNum = Number(n);
+  let dp: number[][] = Array.from(Array(stairNum), () => new Array());
+  // 계단이 한개일때
+  if (stairNum == 1) return inputArr[0];
+  console.log(inputArr);
+  // [i][0] -> i번째 계단을 처음 밟음. [i][1] -> i번째 계단이 두번째로 연속해 밟은 계단
+  dp[0][0] = Number(inputArr[0]);
+  dp[0][1] = 0;
+  dp[1][0] = Number(inputArr[1]);
+  dp[1][1] = Number(inputArr[0] + inputArr[1]);
+  for (let i = 2; i < stairNum; i++) {
+    //첫번째로 밟는 계단일 경우 값 저장
+    dp[i][0] = Math.max(dp[i - 2][0], dp[i - 2][1]) + Number(inputArr[i]);
+    //두번째로 밟는 계단일 경우 값 저장
+    dp[i][1] = dp[i - 1][0] + Number(inputArr[i]);
+  }
+  return Math.max(dp[stairNum - 1][0], dp[stairNum - 1][1]);
+}
 
-solution();
+console.log(solution());
