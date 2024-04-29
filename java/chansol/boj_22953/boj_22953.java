67ad8eb26232f1699f16e0f13915b8c588490290
Elandland <68230815+Elandland@users.noreply.github.com>
2024-04-29 23:15:47 +0900

    Feat. boj 22953 도도의 음식 준비 거의 다품

--- java/chansol/boj_22953/boj_22953.java
+++ java/chansol/boj_22953/boj_22953.java
@@ -3,6 +3,32 @@ import java.util.*;
 
 public class boj_22953 {
 
+    private static long binary_search(int[] arr,int k){
+        long min_time = 0;
+        long max_time = 1000000*1000000/10;
+        long food_cnt;
+        long ans;
+
+        while (min_time <max_time){
+            food_cnt =0;
+            long mid_time = (min_time+max_time)/2;
+
+            for(int i=0; i<arr.length; i++){
+                food_cnt += mid_time/arr[i];            //걸리는 시간 / 1개 만드는 데 걸리는 시간 = i번째 인간이 만드는 총 음식 갯수
+            }
+
+            if(food_cnt<k){         //음식을 더 만들어야 함 ->총 시간 늘려야 함
+                min_time = mid_time;
+            }
+            else{                   //음식이 남을때 -> 시간을 줄여야함.
+                max_time = mid_time+1;
+            }
+
+        }
+        ans = min_time;
+        return ans;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
@@ -27,6 +53,9 @@ public class boj_22953 {
         예쩨 2의 경우 101을 줄였고 3의 경우100을 줄임...
          */
 
+        /*
+        격려 횟수를 정하는데에는 규칙이 없데 -> 격려 횟수가 5번밖에 안됨. -> 경우의 수를 모두 구해서 그냥 이분탐색을 다 돌려버리자
+         */
 
 
         for(int i=0; i<C; i++){
