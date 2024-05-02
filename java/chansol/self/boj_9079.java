0c5086203f4bc127ec643f41b34ae4d2ea0a050f
Elandland <bodan99@gmail.com>
2024-05-02 18:36:05 +0900

    Feat.boj_9079 동전게임 . (개인)

---
+++ java/chansol/self/boj_9079.java
@@ -0,0 +1,25 @@
+package chansol.self;
+
+import java.util.*;
+public class boj_9079 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int[][] coin = new int[T][3];
+
+        for(int i=0; i<T-2;i++ ){
+            if(coin[T%3+i][0]==T){
+
+            }
+        }
+
+        //느낌이 이분탐색 느낌이 살 남.
+        //판별 인덱스는 T%3+i로
+
+
+
+    }
+
+}
