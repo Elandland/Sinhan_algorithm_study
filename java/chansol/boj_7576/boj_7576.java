81dcc1cc4ad82e07dc74aad7b6d0f788cc8757de
Elandland <bodan99@gmail.com>
2024-05-02 18:09:14 +0900

    Feat. boj_7576 solve

--- java/chansol/boj_7576/boj_7576.java
+++ java/chansol/boj_7576/boj_7576.java
@@ -8,75 +8,82 @@ public class boj_7576 {
     private static int N;
     static int min_date = 0;
 
-    private static void bfs(int x, int y, int[][] graph, boolean[][] visited) {
-        Queue<int[]> q = new LinkedList<int[]>();
-        int[] loc = {x, y};
-        q.offer(loc);       //시작하는 위치.
 
-        visited[x][y] = true;
+    private static void bfs(int[][]tomato,boolean[][] visited) {
+        Queue<int[]> q = new LinkedList<>();
+        int oneday_tomato =0; //하루에 동시에 익는 토마토 갯수.
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tomato[i][j] == 1) {
+                    int[] loc = {i, j};
+                    q.offer(loc);
+                }
+            }
+        }           //시작할때 1이 여러개면 q에 여러개가 들어가있는 상태 여야 함.
+
+
+
+
         while (!q.isEmpty()) {
-            int[] locate = q.poll();
-            int i = locate[0];
-            int j = locate[1];
-            int count1 = 0;
-            int count2 = 0;
-            int count3 = 0;
-            int count4 = 0;
 
-            if (i - 1 >= 0) { //좌
-                int[] temp = {i - 1, j};
-                count1 = 0;
-                if (!visited[i - 1][j]) {
-                    count1++;
-                    visited[i - 1][j] = true;
-                    q.offer(temp);
-                }
-            }
-            if (i + 1 < M) {        //우
-                int[] temp = {i + 1, j};
-                count2 = 0;
-                if (!visited[i + 1][j]) {
-                    count2++;
-                    visited[i + 1][j] = true;
-                    q.offer(temp);
-                }
-            }
-            if (j - 1 >= 0) {       //상
-                int[] temp = {i, j - 1};
-                count3 = 0;
-                if (!visited[i][j - 1]) {
-                    count3++;
-                    visited[i][j - 1] = true;
-                    q.offer(temp);
-                }
-            }
-            if (j + 1 < N) {        //하
-                int[] temp = {i, j + 1};
-                count4 = 0;
-                if (!visited[i][j + 1]) {
-                    count4++;
-                    visited[i][j + 1] = true;
-                    q.offer(temp);
-                }
-            }
-            System.out.print(count1);
-            System.out.print(count2);
-            System.out.print(count3);
-            System.out.println(count4);
+            int size = q.size();        //큐 사이즈
+            for(int k=0; k<size; k++) {
+                int[] locate = q.poll();
+                int i = locate[0];
+                int j = locate[1];
 
 
-            min_date += Math.max(Math.max(count1, count2), Math.max(count3, count4));
+                if (i - 1 >= 0) { //좌
+                    int[] temp = {i - 1, j};
+                    if (!visited[i - 1][j]) {
+                        oneday_tomato++;
+                        visited[i - 1][j] = true;
+                        q.offer(temp);
+                    }
+                }
+                if (i + 1 < M) {        //우
+                    int[] temp = {i + 1, j};
+                    if (!visited[i + 1][j]) {
+                        oneday_tomato++;
+                        visited[i + 1][j] = true;
+                        q.offer(temp);
+                    }
+                }
+                if (j - 1 >= 0) {       //상
+                    int[] temp = {i, j - 1};
+                    if (!visited[i][j - 1]) {
+                        oneday_tomato++;
+                        visited[i][j - 1] = true;
+                        q.offer(temp);
+                    }
+                }
+                if (j + 1 < N) {        //하
+                    int[] temp = {i, j + 1};
+                    if (!visited[i][j + 1]) {
+                        oneday_tomato++;
+                        visited[i][j + 1] = true;
+                        q.offer(temp);
+                    }
+                }
+            }
+
+            if (oneday_tomato > 0) {
+                min_date++;
+            }
         }
+
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        M = sc.nextInt();   // 상자의 가로 칸 수
-        N = sc.nextInt();   // 상자의 세로 칸 수
+        N = sc.nextInt();   // 상자의 가로 칸 수  ->아니 이게 행인줄알았네;; 이게 열이네
+        M = sc.nextInt();   // 상자의 세로 칸 수  ->아니 이게 열인줄 알았네, 이게 행이네
 
         int[][] tomato = new int[M][N];
         boolean[][] visit = new boolean[M][N];
+        int check =0;
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
@@ -86,19 +93,30 @@ public class boj_7576 {
                 }
             }
         }
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (tomato[i][j] == 1) {
-                    bfs(i, j, tomato, visit);       //시작점을 tomato의 배열값이 1인 좌표로 시작해야함.
-                }
-            }
+        bfs(tomato, visit);       //시작점을 tomato의 배열값이 1인 좌표로 시작해야함.
+
 
             //bfs
-            // tomato[][] ==1인 애부터 시작해서. 전후좌우 배열 검사. -1이면 검사 끝. 0인애들만 검사하도록 해서 visit한게 M*N - (-1이 있는 갯수) 가 다 되도록.
+            // tomato[][] ==1인 애부터 시작해서. 전후좌우 배열 검사. -1이면 검사 끝. 0인애들만 검사하도록 해서 visit한게 M*N - (-1이 있는 갯수) 가 다 되도록
 
-
+        for(int i=0; i<M; i++){
+            for(int j=0; j<N; j++){
+                if(visit[i][j]){
+                    check++;
+                }
+            }
         }
-        System.out.println(min_date);
+        if(check==(M*N)) {
+            if(min_date>0) {
+                System.out.println(min_date - 1);
+            }
+            else{
+                System.out.println(min_date );
+            }
+        }
+
+        else
+            System.out.println("-1");
     }
 }
 
