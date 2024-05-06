c11292fa3ea5de8a1024b54a7487288caccc372f
Elandland <68230815+Elandland@users.noreply.github.com>
2024-05-06 16:47:23 +0900

    feat. boj_4953 섬의 개수 solve

--- java/chansol/boj_4963/boj_4963.java
+++ java/chansol/boj_4963/boj_4963.java
@@ -3,7 +3,7 @@ package chansol.boj_4963;
 import java.util.*;
 
 public class boj_4963 {
-    static int island_cnt=0;
+    static int island_cnt;
     static int[][] map;
     static boolean[][] visit;
     static int[] dx = {0,0,-1,1,-1,1,1,-1 };                 //상하좌우 왼쪽대각선부터 시계방향
@@ -32,8 +32,10 @@ public class boj_4963 {
 
 
         while(true){
-            h = sc.nextInt();       //몇행인지(세로)
-            w = sc.nextInt();       //몇열인지(가로)
+            island_cnt = 0;
+            h = sc.nextInt();       //몇행인지
+            w = sc.nextInt();       //몇열인지
+
 
             if(w==0&&h==0){         //exit
                 break;
@@ -51,6 +53,8 @@ public class boj_4963 {
 
             for(int i=0; i<w; i++){
                 for(int j=0; j<h; j++) {
+
+
                     if(map[i][j]==1 && !visit[i][j]) {
                         dfs(i, j);       //돌면서 연결된 애들 다 방문하니까 섬 한개가 만들어짐
                         island_cnt++;
