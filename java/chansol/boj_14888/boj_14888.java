4975192d3cd948e33f278da5b17f953a322ee418
Elandland <68230815+Elandland@users.noreply.github.com>
2024-05-09 00:21:59 +0900

    Feat. boj_14888 input

---
+++ java/chansol/boj_14888/boj_14888.java
@@ -0,0 +1,23 @@
+package chansol.boj_14888;
+
+import java.util.*;
+
+public class boj_14888 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] num = new int[N];
+        int[] cal = new int[4];
+        for(int i=0; i<N; i++){
+            num[i] = sc.nextInt();
+        }
+        for(int i=0; i<4; i++){
+            cal[i] = sc.nextInt();      //+, - , *, /
+        }
+
+
+
+    }
+
+}
