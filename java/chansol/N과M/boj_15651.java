1656f7dd421b8cfe32860131995c69ee899d9d40
Elandland <bodan99@gmail.com>
2024-05-23 13:29:05 +0900

    Feat. boj 15651 N과M(3) solve

--- java/chansol/N과M/boj_15651.java
+++ java/chansol/N과M/boj_15651.java
@@ -6,11 +6,13 @@ public class boj_15651 {
     static int N;
     static int M;
     static int[] arr;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
 
-    static String temp="";
     public static void main(String[] args) throws IOException{
         // Scanner sc = new Scanner(System.in);      입력값이 작으면 얘가 더 유리하데. 무조건 버퍼 리더 안써도 될듯?
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //버퍼에 있는 값 전부 출력
+
 
         String s = br.readLine();
 
@@ -23,15 +25,20 @@ public class boj_15651 {
         arr = new int[M];
         back_tracking(0);
 
+        bw.flush();
+        bw.close();
     }
 
-    private static void back_tracking(int depth) {
-        temp ="";
+    private static void back_tracking(int depth) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+
+
         if (depth == M) {
             for(int i=0; i<M; i++) {
-                temp = temp + arr[i] + " ";
+                sb.append(arr[i]).append(" ");
             }
-            System.out.println(temp);
+            bw.write(String.valueOf(sb)+"\n");
             return;
         }
 
